fa12527e150dba01d6d35adb120fb20cd91a4358
luismauricedecastro <mdecastro@fortechint.com>
2023-12-30 20:13:02 -0400

    Revert "Fix"
    This reverts commit 36215d8802404f10834b8a819ae3b795e42c7a0e.

--- atina_raspberrypi/atina.py
+++ atina_raspberrypi/atina.py
@@ -189,13 +189,6 @@ def error_page():
 </html>
 	"""
 
-print("Iniciando web server...")
-try:
-	app.run(host = "0.0.0.0")
-except KeyboardInterrupt:
-	print("Cerrando serial")
-	ser.close()
-
 print("Procesando detections...")
 while display.IsStreaming():
 	# capture the next image
@@ -229,6 +222,13 @@ while display.IsStreaming():
 
 	# update the title bar
 	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+print("Iniciando web server...")
+try:
+	app.run(host = "0.0.0.0")
+except KeyboardInterrupt:
+	print("Cerrando serial")
+	ser.close()
 
 
 '''
